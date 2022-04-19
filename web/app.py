6a8a3f45f19a9e5644719e6f06446d878b931f98
rgm <rgm@nuevayork.local>
2022-04-19 15:24:35 -0400

    Added inception module and Classify resource

--- web/app.py
+++ web/app.py
@@ -1,3 +1,5 @@
+from distutils.log import error
+from os import urandom
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
@@ -45,4 +47,42 @@ class Register(Resource):
             "status": 200,
             "msg": "You successfully signed up for this API"
         }
-        return jsonify(retJson)
+        return jsonify(retJson) 
+
+class Classify(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+        ur       = postedData["url"]
+
+        retJson, error = verifyCredentials(username, password)
+        if error:
+            return jsonify(retJson)
+
+        tokens = users.find({
+            "Username": username
+        })[0]["Tokens"]
+
+        if tokens <= 0:
+            return jsonify (generateReturnDictionary(303, "Not Enough Tokens!"))
+
+        r = requests.get(url)
+        retJson = {}
+        with open("temp.jpg","wb") as f:
+            f.write(r.content)
+            proc = subprocess.Popen('python classify.image.py --model_dir=. --image_file=./temp.jpg')
+            proc.communicate()[0]
+            proc.wait()
+            with open("text.txt") as g:
+                retJson = json.load(g)
+
+        users.update({
+            "Username": username
+        }, {
+            "$set":{
+                "Tokens": tokens-1
+            }
+        })
+        return retJson
