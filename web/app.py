13392fc1bff798cfd63a5421f16db9363c61988e
rgm <rgm@nuevayork.local>
2022-04-24 20:13:51 -0400

    adding imports

--- web/app.py
+++ web/app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 import bcrypt
+import numpy
+import tensorflow as tf
 import requests
 import subprocess
 import json
