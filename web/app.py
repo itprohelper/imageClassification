ad06f0be6f0cc006589a88e3fd237e82293e7a0e
rgm <rgm@nuevayork.local>
2022-04-25 21:00:27 -0400

    fixing classify image resource

--- web/app.py
+++ web/app.py
@@ -103,7 +103,7 @@ class Classify(Resource):
 
         r = requests.get(url)
         retJson = {}
-        with open("temp.jpg","wb") as f:
+        with open("temp.jpg", "wb") as f:
             f.write(r.content)
             proc = subprocess.Popen('python classify_image.py --model_dir=. --image_file=./temp.jpg', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
             ret = proc.communicate()[0]
